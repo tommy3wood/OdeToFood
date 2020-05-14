abb3304389653edbe28b926cb999de2f16c1acfb
tommy3wood <thomaswcurran1014@gmail.com>
2020-05-14 15:55:18 -0500

    stopping point. Need to debut project reference

---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,21 @@
+﻿using OdeToFood.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        public IEnumerable<Restaurant> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
