4fa5db873f34208edfe2c142915e1ad35673178f
tommy3wood <thomaswcurran1014@gmail.com>
2020-05-14 15:36:06 -0500

    project init

---
+++ OdeToFood.Core/CuisineType.cs
@@ -0,0 +1,13 @@
+
+namespace OdeToFood.Core
+{
+    public enum CuisineType
+    {
+        None,
+        Mexican,
+        Italian,
+        Indian
+    }
+
+}
+}
---
+++ OdeToFood.Core/Restaurant.cs
@@ -0,0 +1,14 @@
+
+namespace OdeToFood.Core
+{
+
+    class Restaurant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public CuisineType Cuisine { get; set; }
+    }
+
+}
+}
